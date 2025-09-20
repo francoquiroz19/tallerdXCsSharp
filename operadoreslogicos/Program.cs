d26f89668faf96226b27c94802a3b15df98def6d
FRANCOYAREN <N00500542@upn.pe>
2025-09-20 16:29:46 -0500

    subiendo carpeta de operadores lógicos

---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var (valor1, valor2, valor3) = (true, false, true);
+bool resultand = valor1 && valor2;
+Console.WriteLine(resultand);
+bool resultador = (valor1 && valor2) || valor3;
+Console.WriteLine(resultador);
+bool resutafuerte = (valor1 || valor3) ^ valor2; 
+Console.WriteLine(resutafuerte);
+
+
+
