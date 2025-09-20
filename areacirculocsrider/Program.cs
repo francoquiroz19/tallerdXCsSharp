2f2da4025e2b9b4e03b5b48961976f5086aca5c2
FRANCOYAREN <N00500542@upn.pe>
2025-09-20 16:19:03 -0500

    agregando archivo del area de un circulo desde jbraibs rider en c#

---
+++ areacirculocsrider/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tallerdXCsSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var radio = 0.0;
+            var resultado = 0.0;
+            const double Pi = 3.14159;
+            Console.WriteLine("Calcular area de un circulo"); //ac=pi*radio*radio
+            Console.WriteLine("Ingrese valor del radio: ");
+            radio = double.Parse(Console.ReadLine());
+            resultado = Pi * radio * radio;
+
+            Console.WriteLine($"El area del circulo es {resultado}");
+        }
+    }
+}    
---
+++ tallerdXCsSharp/areadeuncirculp.cs
@@ -0,0 +1,7 @@
+namespace tallerdXCsSharp
+{
+    public class areadeuncirculp
+    {
+        
+    }
+}
